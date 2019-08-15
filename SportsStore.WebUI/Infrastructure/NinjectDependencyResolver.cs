b65acde96250325457675657ec63bfac6e1dd15d
Oleksii Potapov <olexii.potapov@gmail.com>
2019-08-15 10:59:21 +0300

    Created Mock object of product

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Moq;
 using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -19,6 +22,15 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products)
+                .Returns(new List<Product>
+                {
+                    new Product {Name = "Football", Price = 25},
+                    new Product {Name = "Surf board", Price = 179},
+                    new Product {Name = "Running shoes", Price = 95},
+                });
+            _kernel.Bind<IProductRepository>().ToConstant(mock.Object);
         }
 
         public object GetService(Type serviceType)
