9357cf10759d42eae7b2a4fd03e5da1047b6a906
Oleksii Potapov <olexii.potapov@gmail.com>
2019-08-15 15:12:11 +0300

    Added pages to the ProductController List method

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -11,15 +11,19 @@ namespace SportsStore.WebUI.Controllers
     public class ProductController : Controller
     {
         private readonly IProductRepository _repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository repository)
         {
             _repository = repository;
         }
 
-        public ActionResult List()
+        public ActionResult List(int page = 1)
         {
-            return View(_repository.Products);
+            return View(_repository.Products
+                .OrderBy(p => p.ProductId)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize));
         }
     }
 }
