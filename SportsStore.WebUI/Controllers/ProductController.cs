98a5aada3aaf5e3a9f38724ab897f098cada4e89
Oleksii Potapov <olexii.potapov@gmail.com>
2019-08-15 11:13:05 +0300

    Added ProductController

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ActionResult List()
+        {
+            return View(_repository.Products);
+        }
+    }
+}
