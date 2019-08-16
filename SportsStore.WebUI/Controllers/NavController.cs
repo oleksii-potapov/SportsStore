b5231752f4fda90ddbf262c215acb04291929e9d
Oleksii Potapov <olexii.potapov@gmail.com>
2019-08-16 15:36:21 +0300

    Added NavController (controller for categories)

---
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        private IProductRepository _repository;
+
+        public NavController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public PartialViewResult Menu()
+        {
+            return PartialView(_repository.Products
+                .Select(p => p.Category)
+                .Distinct()
+                .OrderBy(c => c));
+        }
+    }
+}
