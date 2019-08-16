8f050fd04728eb26fccaf2e51f3f7a2c36f4678f
Oleksii Potapov <olexii.potapov@gmail.com>
2019-08-16 08:57:21 +0300

    Added test for PageLinks

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
 using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.HtmlHelpers;
 using System.Linq;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.UnitTests
 {
@@ -39,6 +42,32 @@ namespace SportsStore.UnitTests
             Assert.IsTrue(prodArray.Length == 2);
             Assert.AreEqual(prodArray[0].Name, "P4");
             Assert.AreEqual(prodArray[1].Name, "P5");
+        }
+
+        [TestMethod]
+        public void Can_Generate_Page_Links()
+        {
+            // Arrange - define an HTML helper
+            HtmlHelper helper = null;
+
+            // Arrange - create PagingInfo data
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+
+            // Arrange - set up delegate using lambda expression
+            Func<int, string> pageUrlDelegate = i => "Page" + i;
+
+            // Act
+            MvcHtmlString result = helper.PageLinks(pagingInfo, pageUrlDelegate);
+
+            //Assert
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1</a>" +
+                @"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>" +
+                @"<a class=""btn btn-default"" href=""Page3"">3</a>", result.ToString());
         }
     }
 }
