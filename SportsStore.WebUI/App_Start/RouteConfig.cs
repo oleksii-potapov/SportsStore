21d35b991a1c085eb5622b8b819eda0a0fd6ea46
Oleksii Potapov <olexii.potapov@gmail.com>
2019-08-16 15:09:48 +0300

    Edited routes

--- SportsStore.WebUI/App_Start/RouteConfig.cs
+++ SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -14,16 +14,32 @@ namespace SportsStore.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Pages",
-                url: "Page{page}",
-                defaults: new { controller = "Product", action = "List" }
+                null,
+                "",
+                new { controller = "Product", action = "List", category = (string)null, page = 1 }
             );
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Product", action = "List", id = UrlParameter.Optional }
+                null,
+                "Page{page}",
+                defaults: new { controller = "Product", action = "List", category = (string)null },
+                new { page = @"\d+" }
             );
+
+            routes.MapRoute(
+                null,
+                "{category}",
+                new { controller = "Product", action = "List", page = 1 }
+            );
+
+            routes.MapRoute(
+                name: null,
+                url: "{category}/Page{page}",
+                defaults: new { controller = "Product", action = "List" },
+                constraints: new { page = @"\d+" }
+            );
+
+            routes.MapRoute(null, "{controller}/{action}");
         }
     }
 }
