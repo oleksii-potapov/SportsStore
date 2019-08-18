2442483bc81919b4cc526bd571bab48288d34610
Oleksii Potapov <olexii.potapov@gmail.com>
2019-08-18 10:14:10 +0300

    Added EmailOrderProcessor (implementation of IOrderProcessor)

---
+++ SportsStore.Domain/Concrete/EmailOrderProcessor.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.Domain.Concrete
+{
+    public class EmailOrderProcessor : IOrderProcessor
+    {
+        private EmailSettings _settings;
+
+        public EmailOrderProcessor(EmailSettings settings)
+        {
+            _settings = settings;
+        }
+
+        public void ProcessOrder(Cart cart, ShippingDetails shippingDetails)
+        {
+            using (var smtpClient = new SmtpClient())
+            {
+                smtpClient.EnableSsl = _settings.UseSsl;
+                smtpClient.Host = _settings.ServerName;
+                smtpClient.Port = _settings.ServerPort;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential(_settings.UserName, _settings.Password);
+
+                if (_settings.WriteAsFile)
+                {
+                    smtpClient.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+                    smtpClient.PickupDirectoryLocation = _settings.FileLocation;
+                    smtpClient.EnableSsl = false;
+                }
+
+                StringBuilder body = new StringBuilder()
+                    .AppendLine("A new order has been submitted")
+                    .AppendLine("---")
+                    .AppendLine("Items:");
+
+                foreach (var item in cart.Lines)
+                {
+                    var subTotal = item.Quantity * item.Product.Price;
+                    body.AppendFormat("{0} x {1} (subtotal: {2:c}", item.Quantity, item.Product.Name, subTotal);
+                }
+
+                body.AppendFormat("Total order value: {0:c}", cart.ComputeTotalValue())
+                    .AppendLine("---")
+                    .AppendLine("Ship to:")
+                    .AppendLine(shippingDetails.Name)
+                    .AppendLine(shippingDetails.Line1)
+                    .AppendLine(shippingDetails.Line2 ?? "")
+                    .AppendLine(shippingDetails.Line3 ?? "")
+                    .AppendLine(shippingDetails.City)
+                    .AppendLine(shippingDetails.State)
+                    .AppendLine(shippingDetails.Zip)
+                    .AppendLine(shippingDetails.Country)
+                    .AppendLine("---")
+                    .AppendFormat("Gift wrap: {0}", shippingDetails.GiftWrap ? "Yes" : "No");
+
+                MailMessage message = new MailMessage(_settings.MailFromAddress,
+                    _settings.MailToAddress, "New order submitted!", body.ToString());
+
+                if (_settings.WriteAsFile)
+                    message.BodyEncoding = Encoding.ASCII;
+
+                smtpClient.Send(message);
+            }
+        }
+    }
+
+    public class EmailSettings
+    {
+        public string MailToAddress = "orders@example.com";
+        public string MailFromAddress = "sportsstore@example.com";
+        public bool UseSsl = true;
+        public string UserName = "MySmtpUserName";
+        public string Password = "MySmtpPassword";
+        public string ServerName = "smtp.example.com";
+        public int ServerPort = 587;
+        public bool WriteAsFile = false;
+        public string FileLocation = @"c:\sports_store_emails";
+    }
+}
