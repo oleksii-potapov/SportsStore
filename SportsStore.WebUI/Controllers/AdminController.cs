3bdab9149d1d6a35d13e15df552472e54aacd50f
Oleksii Potapov <olexii.potapov@gmail.com>
2019-08-20 11:21:39 +0300

    Added Edit product method (get)

--- SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Controllers
 {
@@ -19,6 +20,12 @@ namespace SportsStore.WebUI.Controllers
         public ViewResult Index()
         {
             return View(_repository.Products);
+        }
+
+        public ViewResult Edit(int productId)
+        {
+            var product = _repository.Products.FirstOrDefault(p => p.ProductId == productId);
+            return View(product);
         }
     }
 }
