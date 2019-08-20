47e15557beb8f2e9d538f4ba0782e40a4be5f2d3
Oleksii Potapov <olexii.potapov@gmail.com>
2019-08-20 13:21:48 +0300

    Added method SaveProduct to the ProductRepository

--- SportsStore.Domain/Concrete/EFProductRepository.cs
+++ SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -12,5 +12,25 @@ namespace SportsStore.Domain.Concrete
     {
         private readonly EFDbContext _context = new EFDbContext();
         public IEnumerable<Product> Products => _context.Products;
+
+        public void SaveProduct(Product product)
+        {
+            if (product.ProductId == 0)
+            {
+                _context.Products.Add(product);
+            }
+            else
+            {
+                var tempProduct = _context.Products.Find(product.ProductId);
+                if (tempProduct == null)
+                    return;
+
+                tempProduct.Name = product.Name;
+                tempProduct.Description = product.Description;
+                tempProduct.Price = product.Price;
+                tempProduct.Category = product.Category;
+            }
+            _context.SaveChanges();
+        }
     }
 }
