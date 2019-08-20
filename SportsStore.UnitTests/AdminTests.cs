3c3c4bdfe063bde30c364b9458663991f1d2d367
Oleksii Potapov <olexii.potapov@gmail.com>
2019-08-20 11:29:42 +0300

    Added Can_Edit_Nonexist_Product test (Admin)

--- SportsStore.UnitTests/AdminTests.cs
+++ SportsStore.UnitTests/AdminTests.cs
@@ -43,5 +43,20 @@ namespace SportsStore.UnitTests
 
             Assert.AreEqual("P2", result.Name);
         }
+
+        [TestMethod]
+        public void Can_Edit_NonExist_Product()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductId = 1, Name="P1"},
+                new Product {ProductId = 2, Name="P2"},
+                new Product {ProductId = 3, Name="P3"},
+            });
+            AdminController target = new AdminController(mock.Object);
+            var result = (Product)target.Edit(4).Model;
+
+            Assert.IsNull(result);
+        }
     }
 }
