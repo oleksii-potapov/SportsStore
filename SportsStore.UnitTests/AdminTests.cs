459dc8b11aebc5dc3a197a7a773645db11ceebcf
Oleksii Potapov <olexii.potapov@gmail.com>
2019-08-20 09:11:17 +0300

    Added AdminController (+test for index)

---
+++ SportsStore.UnitTests/AdminTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class AdminTests
+    {
+        [TestMethod]
+        public void Index_Contains_All_Products()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductId = 1, Name="P1"},
+                new Product {ProductId = 2, Name="P2"},
+                new Product {ProductId = 3, Name="P3"},
+            });
+            AdminController target = new AdminController(mock.Object);
+            var result = (IEnumerable<Product>)target.Index().Model;
+
+            Assert.AreEqual(3, result.Count());
+            Assert.AreEqual("P3", result.ElementAt(2).Name);
+        }
+    }
+}
---
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository _repository;
+
+        public AdminController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ViewResult Index()
+        {
+            return View(_repository.Products);
+        }
+    }
+}
