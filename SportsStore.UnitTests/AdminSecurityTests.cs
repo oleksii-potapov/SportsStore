c0c4a0dc95ca7fd400b0d70c9f559b54cea0a115
Oleksii Potapov <olexii.potapov@gmail.com>
2019-08-21 11:20:17 +0300

    Added tests for authentication

---
+++ SportsStore.UnitTests/AdminSecurityTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.WebUI.Models;
+using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.Infrastructure.Abstract;
+using Moq;
+using System.Web.Mvc;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class AdminSecurityTests
+    {
+        [TestMethod]
+        public void Can_Login_With_Valid_Credentials()
+        {
+            Mock<IAuthProvider> mock = new Mock<IAuthProvider>();
+            mock.Setup(m => m.Authenticate("admin", "secret")).Returns(true);
+            AccountController target = new AccountController(mock.Object);
+            LoginViewModel login = new LoginViewModel
+            {
+                UserName = "admin",
+                Password = "secret"
+            };
+            var result = target.Login(login, "myUrl");
+
+            Assert.IsInstanceOfType(result, typeof(RedirectResult));
+            Assert.AreEqual("myUrl", ((RedirectResult)result).Url);
+        }
+
+        [TestMethod]
+        public void Cannot_Login_With_Invalid_Credentials()
+        {
+            Mock<IAuthProvider> mock = new Mock<IAuthProvider>();
+            mock.Setup(m => m.Authenticate("admin", "secret")).Returns(true);
+            AccountController target = new AccountController(mock.Object);
+            LoginViewModel login = new LoginViewModel
+            {
+                UserName = "wrongUser",
+                Password = "wrongPassword"
+            };
+            var result = target.Login(login, "myUrl");
+
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            Assert.IsFalse(((ViewResult)result).ViewData.ModelState.IsValid);
+        }
+    }
+}
