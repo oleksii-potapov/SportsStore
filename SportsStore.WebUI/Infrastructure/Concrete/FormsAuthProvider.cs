7c33679969e33f115eda30e5be382fe851c4af09
Oleksii Potapov <olexii.potapov@gmail.com>
2019-08-21 10:03:29 +0300

    Added AuthProvider interface and class (will be changed in future)

---
+++ SportsStore.WebUI/Infrastructure/Concrete/FormsAuthProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using SportsStore.WebUI.Infrastructure.Abstract;
+
+namespace SportsStore.WebUI.Infrastructure.Concrete
+{
+    public class FormsAuthProvider : IAuthProvider
+    {
+        public bool Authenticate(string userName, string password)
+        {
+            bool result = FormsAuthentication.Authenticate(userName, password);
+            if (result)
+            {
+                FormsAuthentication.SetAuthCookie(userName, false);
+            }
+            return result;
+        }
+    }
+}
